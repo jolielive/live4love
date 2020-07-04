1e9ad45c64ffac05f6e95f9c5fe3d37b9200e800
jolielive <jolie@live4love.app>
2020-07-04 02:57:11 -0400

    added

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
   siteTitle: 'Gatsby Starter Business', // Site title.
   siteTitleAlt: 'Business', // Alternative site title for SEO.
-  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteLogo: '/icons/iconl4l-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://zen-booth-117b1e.netlify.app', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Showcasing my value, and showing you how to do the same', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
+  disqusShortname: 'live-4-love', // Disqus shortname.
+  userName: 'Jolie Live',
+  userTwitter: 'live4love',
+  userLocation: 'NOVA, VA',
   userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © Live 4 Love Custom Designs. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
